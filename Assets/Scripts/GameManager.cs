a6761b780ee1a46fc5fa3d75e42c12565111b507
stjepanBM <stjepan.bosnjak-matic@racunarstvo.hr>
2023-01-22 00:36:19 +0100

    Respawning

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    //Respawning
+    //Scene manipulation
+
+    private float timeUntilPlayerDies = 2f; 
+
+   public void PlayerRespawn()
+    {
+        StartCoroutine(PlayerRespawnCo());
+    }
+
+
+    IEnumerator PlayerRespawnCo()
+    {
+        yield return new WaitForSeconds(timeUntilPlayerDies);
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+}
--- Assets/Scripts/PlayerHealthSystem.cs
+++ Assets/Scripts/PlayerHealthSystem.cs
@@ -30,6 +30,7 @@ public class PlayerHealthSystem : MonoBehaviour
         if (currentHealth <= 0)
         {
             gameObject.SetActive(false);
+            FindObjectOfType<GameManager>().PlayerRespawn();
         }
     }
 }

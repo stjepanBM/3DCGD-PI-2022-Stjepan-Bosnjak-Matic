d8d1b15770b7ef2469fc80d2cfaff4576dc4cf88
stjepanBM <stjepan.bosnjak-matic@racunarstvo.hr>
2023-01-22 01:33:01 +0100

    Weapons switching system

---
+++ Assets/Scripts/WeaponsSwitchSystem.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponsSwitchSystem : MonoBehaviour
+{
+    private GunSystem activeGun;
+    public List<GunSystem> allGuns = new List<GunSystem>();
+    public int currentGunNumber;
+
+    void Start()
+    {
+        foreach (GunSystem gun in allGuns)
+        {
+            gun.gameObject.SetActive(false);
+        }
+
+        activeGun = allGuns[currentGunNumber];
+        activeGun.gameObject.SetActive(true);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            SwitchGun();
+        }
+    }
+
+    private void SwitchGun()
+    {
+        activeGun.gameObject.SetActive(false);
+        currentGunNumber++;
+
+        if (currentGunNumber >= allGuns.Count)
+        {
+            currentGunNumber = 0;
+        }
+        else
+        {
+            activeGun = allGuns[currentGunNumber];
+            activeGun.gameObject.SetActive(true);
+        }
+    }
+}

37500f83c3cf4a224625ffa5769bd012db6f1ca8
stjepanBM <stjepan.bosnjak-matic@racunarstvo.hr>
2023-01-27 18:24:49 +0100

    levels adjustment

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -101,7 +101,7 @@ public class EnemyAI : MonoBehaviour
         }
 
         Vector3 distanceToDestination = transform.position - destinationPoint;
-        if (distanceToDestination.magnitude < 2f)
+        if (distanceToDestination.magnitude < 3f)
         {
             destinationSet = false;
         }
